6dd517a782a4345d507bc457d0d222b3afdb5e8d
ahmed <ahmed.borhan.ab@gmail.com>
2019-09-07 08:58:25 +0300

    deploy

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/api/admin', admin)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('./client/build'))
     app.get('*', (req, res) => {
-        res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
